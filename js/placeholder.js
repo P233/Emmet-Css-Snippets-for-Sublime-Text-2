45f4e573d614d3a953ef2a18e394222480c71c72
P233 <lvpeiwen@gmail.com>
2012-12-15 14:00:58 +0000

    New snippets list

---
+++ js/placeholder.js
@@ -0,0 +1,23 @@
+// http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+// Released under MIT license: http://www.opensource.org/licenses/mit-license.php
+
+$('[placeholder]').focus(function() {
+  var input = $(this);
+  if (input.val() == input.attr('placeholder')) {
+    input.val('');
+    input.removeClass('placeholder');
+  }
+}).blur(function() {
+  var input = $(this);
+  if (input.val() == '' || input.val() == input.attr('placeholder')) {
+    input.addClass('placeholder');
+    input.val(input.attr('placeholder'));
+  }
+}).blur().parents('form').submit(function() {
+  $(this).find('[placeholder]').each(function() {
+    var input = $(this);
+    if (input.val() == input.attr('placeholder')) {
+      input.val('');
+    }
+  })
+});
